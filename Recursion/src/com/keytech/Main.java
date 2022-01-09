a3914f24aab7b078c5aa0f0c9717aafa03531596
benNjogu <kamandemachafi.gmail.com>
2022-01-09 18:21:14 +0300

    Using recursion

--- Recursion/src/com/keytech/Main.java
+++ Recursion/src/com/keytech/Main.java
@@ -4,24 +4,29 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int num = 3;
-		int factorial = iterativeFactorial(num);
-		System.out.println("Factorial of "+num+" is "+factorial);
+		int num = 7;
+		int factorial = recursiveFactorial(num);
+		System.out.println("Factorial of "+num+" is "+factorial+".");
 		
 	}
 
-	public static int iterativeFactorial(int num) {
+	/*
+	 * 1! = 1 * 0 = 1;
+	 * 2! = 2 * 1 = 2 * 1!;
+	 * 3! = 3 * 2 * 1 = 3 * 2!;
+	 * 4! = 4 * 3 * 2 * 1 = 4 * 3!;
+	 * 
+	 * from the above;
+	 * n! = n * (n-1)!
+	 * 
+	 * */
+	public static int recursiveFactorial(int num) {
 		
 		if (num == 0) {
 			return 1;
 		}
 		
-		int factorial = 1;
-		for(int i = 1; i<=num; i++) {
-			factorial *= i;
-		}
-		
-		return factorial;
+		return num * recursiveFactorial(num - 1);
 	}
 	
 }
