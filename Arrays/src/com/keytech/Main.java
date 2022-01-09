bb777d0c4e5a8a6891aaed7d770a176e21675272
benNjogu <kamandemachafi.gmail.com>
2022-01-09 11:13:22 +0300

    big O comments

--- Arrays/src/com/keytech/Main.java
+++ Arrays/src/com/keytech/Main.java
@@ -3,7 +3,7 @@ package com.keytech;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		int[] intArray = new int[7];
 		intArray[0] = 20;
 		intArray[1] = 35;
@@ -12,18 +12,22 @@ public class Main {
 		intArray[4] = 55;
 		intArray[5] = 1;
 		intArray[6] = -22;
-		
+
 		int index = -1;
-		for(int i=0; i<intArray.length; i++) {
+		for (int i = 0; i < intArray.length; i++) {
 			if (intArray[i] == 7) {
 				index = i;
 				break;
 			}
-			
+
 		}
-		
-		System.out.println("index = "+index);
-		
+
+		System.out.println("index = " + index);
+
+		/*
+		 * When we know the index retrieval is O(1). Retrieval when we don't know the
+		 * index is O(n)
+		 */
 	}
-	
+
 }
