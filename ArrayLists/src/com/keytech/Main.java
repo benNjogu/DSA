8941d4c6d5d7cb98aeb5a44c8ce8a76863fd339b
benNjogu <kamandemachafi.gmail.com>
2022-01-11 16:00:47 +0300

    printing list items

---
+++ ArrayLists/src/com/keytech/Main.java
@@ -0,0 +1,21 @@
+package com.keytech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		List<Employee> employees = new ArrayList<>();
+		
+		employees.add(new Employee(123, "James", "Kamande"));
+		employees.add(new Employee(1234, "Alice", "Kamande"));
+		employees.add(new Employee(1235, "Lemox", "Njogu"));
+		employees.add(new Employee(12367, "Steve", "Jobs"));
+		
+		employees.forEach(employee->System.out.println(employee));
+		
+	}
+
+}
