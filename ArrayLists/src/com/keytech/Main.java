bebf18f79554edbf71ca8cd5b2f9d550cb41ddbe
benNjogu <kamandemachafi.gmail.com>
2022-01-11 16:07:10 +0300

    Replacing items in the list

--- ArrayLists/src/com/keytech/Main.java
+++ ArrayLists/src/com/keytech/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		
 		employees.add(new Employee(123, "James", "Kamande"));
 		employees.add(new Employee(1234, "Alice", "Kamande"));
-		employees.add(new Employee(1235, "Lemox", "Njogu"));
+		employees.add(new Employee(1235, "Lenox", "Njogu"));
 		employees.add(new Employee(12367, "Steve", "Jobs"));
 		
 		employees.forEach(employee->System.out.println(employee));
@@ -19,6 +19,9 @@ public class Main {
 		System.out.println(employees.get(1));
 		System.out.println("-------------");
 		System.out.println(employees.isEmpty());
+		System.out.println("-------------");
+		employees.set(0, new Employee(789, "Kenedy", "Kimanga"));
+		employees.forEach(employee->System.out.println(employee));
 	}
 
 }
