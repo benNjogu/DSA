62d8047c58223aaaaab430ce750ddcf47d0c6cd7
benNjogu <kamandemachafi.gmail.com>
2022-01-11 12:19:30 +0300

    Using parallelSort

--- SortingArraysUsingJDK/src/com/keytech/Main.java
+++ SortingArraysUsingJDK/src/com/keytech/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		
 		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-		Arrays.sort(intArray);
+		Arrays.parallelSort(intArray);
 		
 		for (int i = 0; i < intArray.length; i++) {
 			System.out.println(intArray[i]);
