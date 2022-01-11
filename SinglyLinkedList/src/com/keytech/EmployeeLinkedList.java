ee248a0507072a3303dd58afe65eb49e6126c281
benNjogu <kamandemachafi.gmail.com>
2022-01-11 17:46:03 +0300

    Adding and printing data to and fro a linked list

---
+++ SinglyLinkedList/src/com/keytech/EmployeeLinkedList.java
@@ -0,0 +1,23 @@
+package com.keytech;
+
+public class EmployeeLinkedList {
+
+	private EmployeeNode head;
+	
+	public void addToFront(Employee employee) {
+		EmployeeNode node = new EmployeeNode(employee);
+		node.setNext(head);
+		head = node;
+	}
+	
+	public void printList() {
+		EmployeeNode current = head;
+		System.out.print("HEAD -> ");
+		while(current != null) {
+			System.out.print(current);
+			System.out.print(" -> ");
+			current = current.getNext();
+		}
+		System.out.print("NULL");
+	}
+}
---
+++ SinglyLinkedList/src/com/keytech/Main.java
@@ -0,0 +1,22 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee jamesKamande = new Employee(123, "James", "Kamande");
+		Employee aliceKamande = new Employee(1234, "Alice", "Kamande");
+		Employee lenoxNjogu = new Employee(1235, "Lenox", "Njogu");
+		Employee steveJobs = new Employee(12367, "Steve", "Jobs");
+		
+		EmployeeLinkedList list = new EmployeeLinkedList();
+		list.addToFront(steveJobs);
+		list.addToFront(aliceKamande);
+		list.addToFront(lenoxNjogu);
+		list.addToFront(jamesKamande);
+		
+		list.printList();
+		
+	}
+
+}
