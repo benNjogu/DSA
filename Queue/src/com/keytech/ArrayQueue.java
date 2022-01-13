7ba1786f56497d864aad93190f5a56c7861c18b9
benNjogu <kamandemachafi.gmail.com>
2022-01-13 13:48:19 +0300

    Adding elements to a queue

---
+++ Queue/src/com/keytech/ArrayQueue.java
@@ -0,0 +1,66 @@
+package com.keytech;
+
+import java.util.NoSuchElementException;
+
+public class ArrayQueue {
+
+	private Employee[] queue;
+	private int front;
+	private int back;
+
+	public ArrayQueue(int capacity) {
+		super();
+		queue = new Employee[capacity];
+	}
+	
+	public void enqueue(Employee employee) {
+		if (back == queue.length) {
+			Employee[] newArray = new Employee[2 * queue.length];
+			System.arraycopy(queue, 0, newArray, 0, queue.length);
+			queue = newArray;
+		}
+		
+		queue[back] = employee;
+		back++;
+	}
+	
+	public Employee dequeue() {
+		if (size()==0) {
+			throw new NoSuchElementException();
+		}
+		
+		Employee employee = queue[front];
+		queue[front] = null;
+		front ++;
+		
+		if (size() == 0) {
+			front = 0;
+			back = 0;
+		}
+		
+		return employee;
+	}
+	
+	public Employee peek() {
+		if (size()==0) {
+			throw new NoSuchElementException();
+		}
+		
+		return queue[front];
+	}
+	
+	public int size() {
+		return back - front;
+	}
+	
+	public boolean isEmpty() {
+		return back == 0;
+	}
+	
+	public void printQueue() {
+		for(int i = front; i < back; i++) {
+			System.out.println(queue[i]);
+		}
+	}
+
+}
---
+++ Queue/src/com/keytech/Main.java
@@ -0,0 +1,22 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee jamesKamande = new Employee(123, "James", "Kamande");
+		Employee aliceKamande = new Employee(1234, "Alice", "Kamande");
+		Employee lenoxNjogu = new Employee(1235, "Lenox", "Njogu");
+		Employee steveJobs = new Employee(12367, "Steve", "Jobs");
+		
+		ArrayQueue queue = new ArrayQueue(1);
+		queue.enqueue(jamesKamande);
+		queue.enqueue(aliceKamande);
+		queue.enqueue(lenoxNjogu);
+		queue.enqueue(steveJobs);
+		
+		queue.printQueue();
+		
+	}
+
+}
