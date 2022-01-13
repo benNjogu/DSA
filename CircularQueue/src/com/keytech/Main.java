6f4754f061f6cddc924b004a7519cc632b9e202d
benNjogu <kamandemachafi.gmail.com>
2022-01-13 15:50:39 +0300

    Circular queues

---
+++ CircularQueue/src/com/keytech/Main.java
@@ -0,0 +1,45 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Employee jamesKamande = new Employee(123, "James", "Kamande");
+		Employee aliceKamande = new Employee(1234, "Alice", "Kamande");
+		Employee lenoxNjogu = new Employee(1235, "Lenox", "Njogu");
+		Employee steveJobs = new Employee(12367, "Steve", "Jobs");
+		
+		ArrayQueue queue = new ArrayQueue(1);
+		queue.enqueue(jamesKamande);
+		queue.enqueue(aliceKamande);
+		queue.dequeue();
+		queue.enqueue(lenoxNjogu);
+		queue.dequeue();
+		queue.enqueue(steveJobs);
+		queue.dequeue();
+		queue.enqueue(aliceKamande);
+		queue.dequeue();
+		
+		queue.printQueue();
+		
+		
+//		queue.printQueue();
+//		System.out.println("--------------------------------------------");
+//		queue.dequeue();
+//		queue.dequeue();
+//		queue.printQueue();
+//		System.out.println("--------------------------------------------");
+//		System.out.println(queue.size());
+//		System.out.println("--------------------------------------------");
+//		System.out.println(queue.isEmpty());
+//		System.out.println("--------------------------------------------");
+//		System.out.println(queue.peek());
+//		System.out.println("--------------------------------------------");
+//		queue.dequeue();
+//		queue.dequeue();
+//		queue.dequeue();
+//		queue.printQueue();
+		
+	}
+
+}
