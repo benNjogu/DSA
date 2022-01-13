e164d5f816e9b3439807f6d5a59f05abe5c39e84
benNjogu <kamandemachafi.gmail.com>
2022-01-13 12:18:22 +0300

    Stack palindrome challenge

---
+++ StackUsingLinkesList/src/com/challenge/Main.java
@@ -0,0 +1,38 @@
+package com.challenge;
+
+import java.util.LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(checkForPalindrome("abccba"));
+		System.out.println(checkForPalindrome("Was it a car or a cat I saw"));
+		System.out.println(checkForPalindrome("I did, did I?"));
+		System.out.println(checkForPalindrome("hello"));
+		System.out.println(checkForPalindrome("Don't nod"));
+		System.out.println(checkForPalindrome("ewe"));
+	}
+
+	private static boolean checkForPalindrome(String string) {
+		
+		LinkedList<Character> stack = new LinkedList<Character>();
+		StringBuilder stringNoPunctuation = new StringBuilder(string.length());
+		String lowerCase = string.toLowerCase();
+		
+		for(int i = 0; i < lowerCase.length(); i++) {
+			char c = lowerCase.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				stringNoPunctuation.append(c);
+				stack.push(c);
+			}
+		}
+		
+		StringBuilder reversedString = new StringBuilder(stack.size());
+		while(!stack.isEmpty()) {
+			reversedString.append(stack.pop());
+		}
+
+		return reversedString.toString().equals(stringNoPunctuation.toString());
+	}
+	
+}
