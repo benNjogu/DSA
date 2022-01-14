3fd7131d986a555e630d662e39ddce7412ef4e92
benNjogu <kamandemachafi.gmail.com>
2022-01-14 18:19:04 +0300

    overiding using the key

--- HashtablesJDK/src/com/keytech/Main.java
+++ HashtablesJDK/src/com/keytech/Main.java
@@ -13,12 +13,14 @@ public class Main {
 		Employee aliceKamande = new Employee(1234, "Alice", "Kamande");
 		Employee lenoxNjogu = new Employee(1235, "Lenox", "Njogu");
 		Employee steveJobs = new Employee(12367, "Steve", "Jobs");
+		Employee elon = new Employee(12367, "Elon", "Musk");
 		
 		Map<String, Employee> hTable = new HashMap<String, Employee>();
 		hTable.put("james", jamesKamande);
 		hTable.put("alice", aliceKamande);
 		hTable.put("lenox", lenoxNjogu);
 		hTable.put("steve", steveJobs);
+		hTable.put("steve", elon);
 		
 //		Iterator<Employee> iterator = hTable.values().iterator();
 //		while(iterator.hasNext()) {
