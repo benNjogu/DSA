3261566ff8bd8583036afff2a3355d3cb0612a6d
benNjogu <kamandemachafi.gmail.com>
2022-01-14 18:09:12 +0300

    Printing map values using iterator

---
+++ HashtablesJDK/src/com/keytech/Main.java
@@ -0,0 +1,42 @@
+package com.keytech;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee jamesKamande = new Employee(123, "James", "Kamande");
+		Employee aliceKamande = new Employee(1234, "Alice", "Kamande");
+		Employee lenoxNjogu = new Employee(1235, "Lenox", "Njogu");
+		Employee steveJobs = new Employee(12367, "Steve", "Jobs");
+		
+		Map<String, Employee> hTable = new HashMap<String, Employee>();
+		hTable.put("james", jamesKamande);
+		hTable.put("alice", aliceKamande);
+		hTable.put("lenox", lenoxNjogu);
+		hTable.put("steve", steveJobs);
+		
+		Iterator<Employee> iterator = hTable.values().iterator();
+		while(iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+		
+		System.out.println("Retrieve key steve-> "+hTable.get("steve"));
+		System.out.println("---------------------------------------------------------");
+		hTable.remove("steve");
+		Iterator<Employee> iterator1 = hTable.values().iterator();
+		while(iterator1.hasNext()) {
+			System.out.println(iterator1.next());
+		}
+		System.out.println("---------------------------------------------");
+//		hTable.remove("alice");
+//		hTable.printHashtable();
+//		System.out.println("---------------------------------------------");
+//		System.out.println("Retrieve key lenox-> "+hTable.get("lenox"));
+	}
+
+}
