83c3f3e9ea286229a35e9849482a60b3053567a1
benNjogu <kamandemachafi.gmail.com>
2022-01-15 10:12:33 +0300

    Linear search

---
+++ LinearSearch/src/com/keytech/Main.java
@@ -0,0 +1,24 @@
+package com.keytech;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int[] intArray = {20,35,-15,7,55,1,-22};
+		
+		System.out.println(linearSearch(intArray, 7));
+		System.out.println(linearSearch(intArray, 13));
+		
+	}
+	
+	public static int linearSearch(int[] input, int value) {
+		for (int i = 0; i < input.length; i++) {
+			if (input[i] == value) {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+
+}
