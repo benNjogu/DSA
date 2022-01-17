20da916722a122711d9613827b3148bfc55009f4
benNjogu <kamandemachafi.gmail.com>
2022-01-17 14:10:12 +0300

    Inserting elements into a heap

---
+++ MaxHeap/src/com/keytech/Heap.java
@@ -0,0 +1,42 @@
+package com.keytech;
+
+public class Heap {
+
+	private int[] heap;
+	private int size;
+
+	public Heap(int capacity) {
+		super();
+
+		heap = new int[capacity];
+	}
+	
+	public void insert(int value) {
+		if (isFull()) {
+			throw new IndexOutOfBoundsException("Heap is full");
+		}
+		
+		heap[size] = value;
+		fixHipAbove(size);
+		size++;
+	}
+
+	private void fixHipAbove(int index) {
+		int newValue = heap[index];
+		while(index > 0 && newValue > heap[getParent(index)]) {
+			heap[index] = heap[getParent(index)];
+			index = getParent(index);
+		}
+		
+		heap[index] = newValue;
+	}
+	
+	public boolean isFull() {
+		return size == heap.length;
+	}
+	
+	public int getParent(int index) {
+		return (index - 1) / 2;
+	}
+	
+}
